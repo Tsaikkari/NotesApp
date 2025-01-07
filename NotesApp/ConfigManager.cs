79d77c57aec767920bd58e94cd21ea4f2d0ab088
Kirsi Trospe <kirsi.trospe@gmail.com>
2025-01-07 06:53:28 +0100

    Add parser

---
+++ NotesApp/ConfigManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace NotesApp
+{
+    public static class ConfigManager
+    {
+        public static JObject LoadStyleDonfig()
+        {
+            if (File.Exists("StyleConfig.json"))
+            {
+                string config = File.ReadAllText("StyleConfig.json");
+                return JObject.Parse(config);
+            }
+            return null;
+        }
+    }
+}
